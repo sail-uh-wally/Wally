b5b84365ec0e1381fe769dfccc084f83a490a3b2
polonovo <84686861+polonovo@users.noreply.github.com>
2024-01-30 20:47:01 -0600

    AXIOM

--- AXIOM.py
+++ AXIOM.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+
 def straight(PresPos,NewPos, step):
     
 # Calculating the x and y changes in position
@@ -26,48 +27,73 @@ def straight(PresPos,NewPos, step):
         
     return X,Y
 
-X = []
-Y = []
+def StraightLines(X,Y):
+    Xp = []
+    Yp = []
+    for i in range(1,len(X)):
+        Xs,Ys = straight([X[i-1],Y[i-1]],[X[i],Y[i]],2)
+        for j in range(len(Xs)):
+            Xp.append(Xs[j])
+            Yp.append(Ys[j])
+    return Xp,Yp
 
-# A
+def AXIOM():
 
-XA = [-100,-60,-60]
-YA = [220,280,220]
+    X = []
+    Y = []
 
-X.append(XA)
-Y.append(YA)
+    # A
 
-# X
+    XA = [-100,-60,-60]
+    YA = [220,280,220]
 
-XX = [-50,-25,-5,-25,-50,-5]
-YX = [220,250,280,250,280,220]
+    for i in range(len(XA)):
+        X.append(XA[i])
+        Y.append(YA[i])
 
-X.append(XX)
-Y.append(YX)
+    # X
+
+    XX = [-50,-27.5,-5,-27.5,-50,-5]
+    YX = [220,250,280,250,280,220]
+
+    for i in range(len(XX)):
+      X.append(XX[i])
+      Y.append(YX[i])
 
 # I
 
-XI = [5,5,5]
-YI = [220,280,220]
+    XI = [5,5,5]
+    YI = [220,280,220]
 
-X.append(XI)
-Y.append(YI)
+    for i in range(len(XI)):
+       X.append(XI[i])
+       Y.append(YI[i])
 
-# O
+    # O
 
-XO = [15,15,45,45,15,45]
-YO = [220,280,280,220,220,220]
+    XO = [15,15,45,45,15,45]
+    YO = [220,280,280,220,220,220]
 
-X.append(XO)
-Y.append(YO)
+    for i in range(len(XO)):
+        X.append(XO[i])
+        Y.append(YO[i])
 
 # M
 
-XM = [55,55,75,80,100,100]
-YM = [220,280,260,260,280,220]
+    XM = [55,55,75,80,100,100]
+    YM = [220,280,260,260,280,220]
 
-X.append(XM)
-Y.append(YM)
+    for i in range(len(XM)):
+        X.append(XM[i])
+        Y.append(YM[i])
+
+    X,Y = StraightLines(X,Y)
+    return X,Y
+
+X,Y = AXIOM()
+
+print(X)
+print(Y)
 
 fig,ax = plt.subplots()
 ax.plot(X,Y)
